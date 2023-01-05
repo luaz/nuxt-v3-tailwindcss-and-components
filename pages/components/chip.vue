7a0a0aceaf39cbfafc29a5e54850d28d10093763
luaz <desmond.lua@gmail.com>
2023-01-05 17:35:03 +0800

    add chip style sample

---
+++ pages/components/chip.vue
@@ -0,0 +1,24 @@
+<script setup>
+useHead({
+  title: 'Chip'
+})
+
+const tags = ['apple', 'banana', 'orange', 'grape', 'strawberry', 'mango', 'pear', 'pineapple', 'watermelon', 'lemon', 'peach', 'apricot', 'plum', 'cherry', 'blueberry', 'raspberry', 'blackberry', 'kiwi', 'cantaloupe', 'avocado', 'star fruit', 'papaya', 'dragon fruit', 'persimmon']; 
+</script>
+
+<template>
+  <div>
+    <h1 class="text-4xl">Chip</h1>
+    <ul class="inline-flex flex-wrap gap-x-2 gap-y-1 my-3">
+      <li v-for="tag in tags" :key="tag" class="px-2 py-1 bg-gray-300 rounded-lg">
+        {{ tag }}
+      </li>
+    </ul>
+
+    <ul class="inline-flex flex-wrap gap-x-2 gap-y-1 my-3">
+      <li v-for="tag in tags" :key="tag" class="px-2 py-1 bg-gray-100 rounded-lg">
+        {{ tag }}
+      </li>
+    </ul>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl">Hello World</h1>
     <div class="text-red-500">text-red-500</div>
+
+    <h2 class="text-3xl">Components</h2>
+    <ul>
+      <li><NuxtLink to="/components/chip">Chip</NuxtLink></li>
+    </ul>
   </div>
 </template>
