b05eada1426358263ada2ddf97efc966f659060a
luaz <desmond.lua@gmail.com>
2023-01-05 16:53:22 +0800

    setup sample page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['~/assets/styles/main.css'],
+  app: {
+    head: {
+      title: 'Nuxt3 with Tailwind CSS'
+    }
+  },    
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-4xl">Hello World</h1>
+    <div class="text-red-500">text-red-500</div>
+  </div>
+</template>
