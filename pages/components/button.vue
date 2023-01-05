7f998bd54576d81a117207d8ae3b71144f394f72
luaz <desmond.lua@gmail.com>
2023-01-05 19:25:18 +0800

    add button tailwind component

--- pages/components/button.vue
+++ pages/components/button.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Button</h1>
+
+    <h2>By Utilities</h2>
     <div class="t-inline-container my-3">
       <button type="button" class="px-4 py-2 font-medium rounded-lg border border-gray-200 hover:bg-gray-100">
         Button
@@ -23,7 +25,34 @@
       </NuxtLink>
 
       <NuxtLink class="w-fit cursor-pointer m-0 px-4 py-2 font-medium rounded-lg border border-gray-200 hover:bg-gray-100 button-link">
-        Nuxt Link (Button Link)
+        Link (Button Link)
+      </NuxtLink>
+    </div>
+
+    <h2>By Component</h2>
+    <div class="t-inline-container my-3">
+      <button type="button" class="t-button">
+        Button
+      </button>
+
+      <button type="button" class="t-button t-button-outline">
+        Outline Button
+      </button>
+
+      <button type="button" class="t-button t-button-fill">
+        Outline Filled
+      </button>
+
+      <NuxtLink class="t-button">
+        Link
+      </NuxtLink>
+
+      <NuxtLink class="t-button no-link">
+        Link (No Link)
+      </NuxtLink>
+
+      <NuxtLink class="t-button button-link">
+        Link (Button Link)
       </NuxtLink>
     </div>
   </div>
